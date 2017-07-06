f96f78db3e5f05f10897fada6fa0549236fa8b96
levicash <472126511@163.com>
2017-07-06 17:09:28 +0800

    后台加入中奖维护

---
+++ Web_Project.View/admin_/Prize/Info.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web_Project.Data;
+
+
+namespace Web_Project.View.admin_.Prize
+{
+    public partial class Info : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                HttpContext.Current.Session["ManagerPageUrl"] = HttpContext.Current.Request.Url.OriginalString;
+                if (AdminInfo.UserID == null || AdminInfo.UserID == "" || AdminInfo.UserID.Equals(""))
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "", "<script language=javascript>alert('已退出登录！');top.location='../login.aspx'</script>");
+                }
+                else
+                {
+                    ShowInfo();
+                }
+            }
+        }
+        /// <summary>
+        /// 信息绑定
+        /// </summary>
+        protected void ShowInfo()
+        {
+            string sql = "select * from ws_Advertising order by Orders,Id";
+            DataTable db = DBHelpers.ExecuteDataTable(DBHelpers.conn, CommandType.Text, sql, null);
+            Repeater1.DataSource = db;
+            Repeater1.DataBind();
+        }
+    }
+}
